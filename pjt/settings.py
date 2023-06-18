2bf4a7a5de07a94f781df859d40172f8cace70b1
forwardyoung <chahwayoung214@gmail.com>
2023-06-19 00:12:52 +0900

    create templates base file

--- pjt/settings.py
+++ pjt/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = "pjt.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
